55827f249c91445c022421669f7c1df60f446670
unknown <darrius.clk@gmail.com>
2021-06-26 17:26:45 +0800

    continue calculate funtion added

--- cal.py
+++ cal.py
@@ -13,3 +13,22 @@ elif op == '*':
 elif op == '/':
 	ans = float(x)/float(y)
 	print('the answer is: ', ans)
+b = ans
+z = input('do u want to continue calculating with the answer (yes/no): ')
+if z == 'yes':
+	w = input('please input the no. : ')
+	op2 = input('what operation would u like (+, -, *, /) :')
+	if op2 == '+':
+		c = b+float(w)
+		print('the answer is: ', c)
+	elif op2 == '-':
+		c = b-float(w)
+		print('the answer is: ', c)
+	elif op2 == '*':
+		c = b*float(w)
+		print('the answer is: ', c)
+	elif op2 == '/':
+		c = b/float(w)
+		print('the answer is: ', c)
+if z == 'no':
+	print('thankyou.')
