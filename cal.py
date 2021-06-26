76869cd4b24c09ddd0aaf5b377637148c391f2d7
unknown <darrius.clk@gmail.com>
2021-06-26 14:32:51 +0800

    first version with addition function operation

---
+++ cal.py
@@ -0,0 +1,6 @@
+x = input('please input the first no. : ')
+y = input('please input the second no.: ')
+op = input('do u want to get the sum (y/n) :')
+if op == 'y':
+	ans = float(x)+float(y)
+	print('your ans is: ', ans)
